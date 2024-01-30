c3080f49c151ad88076a9e0be4adcb5be0345716
Luis Angel <luisangel.fnz@gmail.com>
2024-01-30 00:10:09 -0600

    Se agregan configuraciones para poder utilizar media solo DEV

--- EmpresarialWeb/settings.py
+++ EmpresarialWeb/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
+import os
 
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'MediaDev/'
+
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'MediaDEV')
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
 urlpatterns = [
     path('',  views.index ),
     path('index/',  views.index, name='index'),
@@ -11,3 +11,10 @@ urlpatterns = [
     path('services/', views.services, name='services'),
     path('store/', views.store, name='store'),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    
