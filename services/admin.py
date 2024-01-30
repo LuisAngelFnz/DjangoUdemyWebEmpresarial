f6ce80eab8d850dccefe2c09accda706c82c8130
Luis Angel <luisangel.fnz@gmail.com>
2024-01-30 01:18:56 -0600

    Agregada app services al administrador de django

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Service
 # Register your models here.
+class ServicesAdmin(admin.ModelAdmin):
+    readonly_fields = ['create','update']
+
+admin.site.register(Service, ServicesAdmin)
