f7a0411f5e61a29ae64e90103f97f4a461d85d97
Luis Angel <luisangel.fnz@gmail.com>
2024-02-05 22:33:43 -0600

    Primer migración de app pages

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+    title   = models.CharField(verbose_name='Titulo', max_length=200)
+    content = models.TextField(verbose_name='Contenido')
+    create  = models.DateTimeField(verbose_name='Fecha de creación', auto_now_add=True)
+    update  = models.DateTimeField(verbose_name='Fecha de actualización', auto_now=True)
+
+    class Meta:
+        verbose_name = 'pagina'
+        verbose_name_plural = 'paginas'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
