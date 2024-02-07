bae54c8182002ae6963dec681bf8d210fa04a8f3
Luis Angel <luisangel.fnz@gmail.com>
2024-02-06 23:58:44 -0600

    Cambiar ModelField de content para habilitar editor wysiwyg

--- pages/models.py
+++ pages/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Page(models.Model):
     title   = models.CharField(verbose_name='Titulo', max_length=200)
-    content = models.TextField(verbose_name='Contenido')
+    content = RichTextField(verbose_name='Contenido')
     order   = models.SmallIntegerField(verbose_name='Order', default=0)
     create  = models.DateTimeField(verbose_name='Fecha de creación', auto_now_add=True)
     update  = models.DateTimeField(verbose_name='Fecha de actualización', auto_now=True)
