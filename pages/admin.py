0446c6dd2d61d4d23aece864bf1097866e238b60
Luis Angel <luisangel.fnz@gmail.com>
2024-02-06 22:34:40 -0600

    Agregar orden a las pages

--- pages/admin.py
+++ pages/admin.py
@@ -4,3 +4,4 @@ from .models import Page
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
     readonly_fields = ['create', 'update']
+    list_display = ('title','order')
--- pages/models.py
+++ pages/models.py
@@ -3,13 +3,14 @@ from django.db import models
 class Page(models.Model):
     title   = models.CharField(verbose_name='Titulo', max_length=200)
     content = models.TextField(verbose_name='Contenido')
+    order   = models.SmallIntegerField(verbose_name='Order', default=0)
     create  = models.DateTimeField(verbose_name='Fecha de creación', auto_now_add=True)
     update  = models.DateTimeField(verbose_name='Fecha de actualización', auto_now=True)
 
     class Meta:
         verbose_name = 'pagina'
         verbose_name_plural = 'paginas'
-        ordering = ['title']
+        ordering = ['order','title']
 
     def __str__(self):
         return self.title
