e459cf7174f92e207b71d6a5e58001a189ef1df8
Luis Angel <luisangel.fnz@gmail.com>
2024-02-19 23:11:32 -0600

    Agregar funcionalidad para el envio de correos en la app contact

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
 from .forms import ContactForm
 
 def contact(request):
@@ -6,9 +7,20 @@ def contact(request):
         objform = ContactForm(data=request.POST)
         print(objform.is_valid())
         if objform.is_valid():
-            name, email, content = map(request.POST.get, ('name','email','content'))
-            #Todo ok
-            return redirect('/contact/?ok')
+            name, mail, content = map(request.POST.get, ('name','email','content'))
+            # Enviar mensaje
+            email = EmailMessage(
+                'La Caffettiera : Nuevo mensaje de contacto',
+                'De {} <{}> \n\nEscribio:\n\n{}'.format(name, mail, content),
+                'no-contestar@inbox.mailtrap.io',
+                ['lfernandez4gds@gmail.com'],
+                reply_to=[mail]
+            )
+            try:
+                email.send()
+                return redirect('/contact/?ok')
+            except:
+                return redirect('/contact/?fail')
     else:
         objform = ContactForm()
     # objform = ContactForm()
