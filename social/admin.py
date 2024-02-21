8c3e652973024969c9f1b9333d8e0b93be8297c5
Luis Angel <luisangel.fnz@gmail.com>
2024-02-20 23:51:40 -0600

    Agregar metodo get_readonly_fields en el admin de AppSocial

--- social/admin.py
+++ social/admin.py
@@ -1,6 +1,15 @@
+from typing import Any
 from django.contrib import admin
+from django.http.request import HttpRequest
 from .models import Social
 
 @admin.register(Social)
 class SocialAdmin(admin.ModelAdmin):
-    readonly_fields = ['create','update']    
+    readonly_fields = ['create','update']
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.readonly_fields
+        
+        return ['key','name']
+        
